84c0600d53c28c43f64e41f3e74167ee35a67969
Zane Paton <zanerepaton@gmail.com>
2019-12-06 09:59:39 -0600

    add css prefix function

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 const gulp = require("gulp");
-const prefix = require("auto-prefixer");
-const cleancss = require("gulp-clean-css");
-const imagemin = require("gulp-imagemin");
-const babel = require("gulp-babel");
-const concat = require("gulp-concat");
-const ugly = require("gulp-uglify");
+const prefix = require("gulp-autoprefixer");
+// const cleancss = require("gulp-clean-css");
+// const imagemin = require("gulp-imagemin");
+// const babel = require("gulp-babel");
+// const concat = require("gulp-concat");
+// const ugly = require("gulp-uglify");
+
+function autoprefix() {
+  return gulp
+    .src("src/css/style.css")
+    .pipe(
+      prefix({
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("dest/css"));
+}
+
+exports.all = gulp.series(autoprefix);
