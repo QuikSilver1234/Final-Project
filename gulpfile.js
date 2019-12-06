3466d22b6dc1c14d34a9593e1b114262e450b69f
Zane Paton <zanerepaton@gmail.com>
2019-12-06 10:12:57 -0600

    add new function to existing css function

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require("gulp");
 const prefix = require("gulp-autoprefixer");
-// const cleancss = require("gulp-clean-css");
+const cleancss = require("gulp-clean-css");
 // const imagemin = require("gulp-imagemin");
 // const babel = require("gulp-babel");
 // const concat = require("gulp-concat");
 // const ugly = require("gulp-uglify");
 
-function autoprefix() {
+function css() {
   return gulp
     .src("src/css/style.css")
     .pipe(
@@ -14,7 +14,9 @@ function autoprefix() {
         cascade: false
       })
     )
+    .pipe(cleancss({ compatibility: "ie8" }))
+
     .pipe(gulp.dest("dest/css"));
 }
 
-exports.all = gulp.series(autoprefix);
+exports.all = gulp.series(css);
