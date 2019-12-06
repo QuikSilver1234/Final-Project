c7106447ae362396ac0052aa1ffcb7f5252b051e
Zane Paton <zanerepaton@gmail.com>
2019-12-06 09:22:55 -0600

    install necessary packages for project

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require("gulp");
+const prefix = require("auto-prefixer");
+const cleancss = require("gulp-clean-css");
+const imagemin = require("gulp-imagemin");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+const ugly = require("gulp-uglify");
