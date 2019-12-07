f02273d4b08b64c510d01ca89ec0f4b5fbff19b2
Zane Paton <zanerepaton@gmail.com>
2019-12-06 18:09:11 -0600

    fix an issue in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require("gulp");
 const prefix = require("gulp-autoprefixer");
 const cleancss = require("gulp-clean-css");
 const imagemin = require("gulp-imagemin");
-// const babel = require("gulp-babel");
+const babel = require("gulp-babel");
 // const concat = require("gulp-concat");
 // const ugly = require("gulp-uglify");
 
